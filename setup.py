47ec94ecdc6262b0268f551daea0cbaa30dd0726
Michał Węgrzynek <michal.wegrzynek@malloc.com.pl>
2019-01-19 23:05:50 +0100

    PyPI publication

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
 # -*- coding: UTF-8 -*-
-
+import pathlib
 from setuptools import find_packages, setup
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+VERSION = '0.0.1'
 
 setup(
     name='pysupla',
     description="Simple Supla's OpenAPI wrapper",
-    version='0.0.1',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Michal Wegrzynek',
+    author_email='michal.wegrzynek@malloc.com.pl',
+    url='https://github.com/mwegrzynek/pysupla',
+    license='Apache License',
+    version=VERSION,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Home Automation'
+    ],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'aiohttp',
         'pytest'
-    ],
-
+    ]
 )
