725a72313737704361a90bded22acbb2d9fce567
Michał Węgrzynek <michal.wegrzynek@malloc.com.pl>
2020-08-15 18:50:19 +0200

    Requirements fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 setup(
     name='pysupla',
@@ -27,7 +27,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'aiohttp',
-        'pytest'
+        'requests'
     ]
 )
