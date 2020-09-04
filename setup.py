88e871758e46279e2bc04674861b826a5ff215f2
Michał Węgrzynek <michal.wegrzynek@malloc.com.pl>
2020-09-04 13:31:07 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
-VERSION = '0.0.4'
+VERSION = '0.0.5'
 
 setup(
     name='pysupla',
